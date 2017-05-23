a9a2004a262cbd554b821128bb407f4d08326666
Raj Singh <raj@foreversingh.com>
2017-05-22 20:51:26 -0500

    successfully bound an action creator and verified it fires in console

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,9 @@ export default function () {
     { title: 'Javascript: The Good Parts' },
     { title: 'Harry Potter' },
     { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'Eloquent Ruby' },
+    { title: 'The Last Jedi' },
+    { title: 'Empire at War' },
+    { title: 'A History of the Peloponnesian War' }
   ]
 }
