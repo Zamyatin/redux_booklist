ef55d3ce0d4fbc7dc01a6ab1d476bf80972b284c
Raj Singh <raj@foreversingh.com>
2017-05-24 16:08:08 -0500

    added active book reducer, details, and activebook detail component

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,11 +1,10 @@
 export default function () {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' },
-    { title: 'The Last Jedi' },
-    { title: 'Empire at War' },
-    { title: 'A History of the Peloponnesian War' }
+    { title: 'Javascript: The Good Parts', author: 'Douglas Crockford', pages: 85 },
+    { title: 'Harry Potter', author: 'J.K. Rowling', pages: 304 },
+    { title: 'The Dark Tower', author: 'Stephen King', pages: 743 },
+    { title: 'Eloquent Ruby', author: 'Russsel Olsen', pages: 183 },
+    { title: 'The Last Jedi', author: 'Rian Johnson', pages: 432 },
+    { title: 'A History of the Peloponnesian War', author: 'Thucydides', pages: 643 }
   ]
 }
